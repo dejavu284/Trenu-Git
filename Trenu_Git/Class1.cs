d8ced54fe9ae0ddac7f2a6fba74d8c80f9ec1b5e
Alexandr <skuratov.1411@gmail.com>
2022-06-03 12:51:31 +0700

    first commit

---
+++ Trenu_Git/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trenu_Git
+{
+    class Class1
+    {
+        private string pole = "trenim git";
+        public void PrintPole()
+        {
+            Console.WriteLine(pole);
+        }
+    }
+}
---
+++ Trenu_Git/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Trenu_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            class1.PrintPole();
+        }
+    }
+}
